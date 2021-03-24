fa868972422842aaf12f35ee3fb25c7935405391
ravi26192 <chauhanravi26192@gmail.com>
2021-03-24 15:46:56 -0400

    Display User Chat Rooms

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View, Image, TouchableWithoutFeedback } from 'react-native';
 // import Avatar from 'react-avatar';
 import { ChatRoom } from '../../types';
 import styles from './styles';
 import moment from 'moment';
 import { useNavigation } from '@react-navigation/native';
+import Auth from '@aws-amplify/auth';
+import { useState } from 'react';
 
 export type ChatListItemProps = {
     chatRoom: ChatRoom;
@@ -13,31 +15,47 @@ export type ChatListItemProps = {
 const ChatListItem = (props: ChatListItemProps) => {
     const { chatRoom } = props;
 
-    const navigation = useNavigation();
+    const [ otherUser, setOtherUser] = useState(null);
 
-    const user = chatRoom.users[1];
+    const navigation = useNavigation();    
+
+    useEffect( () => {
+        const getOtherUser = async () => {
+            const userInfo = await Auth.currentAuthenticatedUser();
+            if(chatRoom.chatRoomUsers.items[0].user.id === userInfo.attributes.sub) {
+                setOtherUser(chatRoom.chatRoomUsers.items[1].user);
+            } else {
+                setOtherUser(chatRoom.chatRoomUsers.items[0].user);
+            }
+        }
+        getOtherUser();
+    }, [])
 
     const onClick = () => {
         navigation.navigate('ChatRoom', {
             id: chatRoom.id,
-            name: user.name,
+            name: otherUser.name,
         })
-    } 
+    }
+
+    if(!otherUser) {
+        return null;
+    }
 
     return (
         <TouchableWithoutFeedback onPress={onClick}>
             <View style={styles.container}>
             <View style={styles.leftContainer}>
-                <Image source={{ uri: user.imageUri }} style={styles.avatar} />
+                <Image source={{ uri: otherUser.imageUri }} style={styles.avatar} />
 
                 <View style={styles.midContainer}>
-                    <Text style={styles.username}> {user.name} </Text>
-                    <Text numberOfLines={1} style={styles.lastMessage}> {chatRoom.lastMessage.content} </Text>
+                    <Text style={styles.username}> {otherUser.name} </Text>
+                    <Text numberOfLines={1} style={styles.lastMessage}> {chatRoom.lastMessage ? chatRoom.lastMessage.content : ""} </Text>
                 </View>    
             </View>
 
                 <Text style={styles.time}> 
-                    {moment(chatRoom.lastMessage.createdAt).format('DD/MMM/YYYY')}
+                    {chatRoom.lastMessage && moment(chatRoom.lastMessage.createdAt).format('DD/MMM/YYYY')}
                 </Text>
             </View>
         </TouchableWithoutFeedback>
--- screens/ChatsScreen.tsx
+++ screens/ChatsScreen.tsx
@@ -4,14 +4,43 @@ import { Text, View } from '../components/Themed';
 import ChatListItem from '../components/ChatListItem';
 import chatRooms from '../data/ChatRooms';
 import NewMessageButton from '../components/NewMessageButton';
+import { useEffect } from 'react';
+import { Auth, API, graphqlOperation } from 'aws-amplify';
+import { getUser } from './queries';
+import { useState } from 'react';
 
 export default function ChatsScreen() {
+
+  const [chatRooms, setChatRooms] = useState([]);
+
+  useEffect( () => {
+    const fetchChatRooms = async () => {
+      try{
+        const userInfo = await Auth.currentAuthenticatedUser();
+        const userData = await API.graphql(
+          graphqlOperation(
+            getUser, {
+              id: userInfo.attributes.sub,
+            }
+          )
+        )
+
+        setChatRooms(userData.data.getUser.chatRoomUser.items);
+        console.log(userData.data.getUser.chatRoomUser.items);
+
+      } catch (e){
+        console.log(e);
+      }
+    }
+    fetchChatRooms();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
         style={{width:'100%'}}
         data={chatRooms}
-        renderItem={({item}) => <ChatListItem chatRoom={item}/>}
+        renderItem={({item}) => <ChatListItem chatRoom={item.ChatRoom}/>}
         keyExtractor={(item) => item.id}
       />
       <NewMessageButton />
